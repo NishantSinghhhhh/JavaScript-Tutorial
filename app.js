5f37107211569eff6bc827484658309c9eddbd73
NishantSinghhhhh <151461374+NishantSinghhhhh@users.noreply.github.com>
2024-02-16 22:15:32 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+//  document.write('Hello World');
+//  alert('hello World');
+//  console.log('hello World');
+
+// variables - most Basic Building Block
+// variables - StorageEvent, Access , modify === Value
+// Declare , assignment Operator , assign Value 
+
+let name = "Nishant Singh";
+console.log({name});
